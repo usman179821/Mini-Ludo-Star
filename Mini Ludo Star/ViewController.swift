0cef7fc53282c00834acc4f18f474c0db3fa59ae
usman <muhammadusman17982@gmail.com>
2022-05-31 12:17:10 +0500

    change the ludo star application

---
+++ Mini Ludo Star/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Mini Ludo Star
+//
+//  Created by Admin on 31/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    //Mark: - variable
+    var diceNameArr = [ UIImage(named: "DiceOne"),
+                       UIImage(named: "DiceTwo"),
+                       UIImage(named: "DiceThree"),
+                       UIImage(named: "DiceFour"),
+                       UIImage(named: "DiceFive"),
+                       UIImage(named: "DiceSix"),
+    ]
+    
+    @IBOutlet weak var DiceImgOne: UIImageView!
+    @IBOutlet weak var DiceImgTwo: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func RollBtnTapped(_ sender: Any) {
+        
+        let index = Int.random(in: 0..<diceNameArr.count)
+        DiceImgOne.image = diceNameArr[index]
+        DiceImgTwo.image = diceNameArr[index]
+          
+    }
+}
